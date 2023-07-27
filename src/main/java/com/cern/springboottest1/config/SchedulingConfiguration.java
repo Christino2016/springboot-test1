6d8edfb149616f988f426f31d936c7978d287557
lenovo <phantom_xf@hotmail.com>
2023-07-27 15:37:37 +0800

    1

--- src/main/java/com/cern/springboottest1/config/SchedulingConfiguration.java
+++ src/main/java/com/cern/springboottest1/config/SchedulingConfiguration.java
@@ -9,7 +9,7 @@ import java.util.Date;
 public class SchedulingConfiguration {
 
     //@Scheduled(fixedRate = 5000)
-    @Scheduled(cron = "*/10 * * * * ?")
+    @Scheduled(cron = "*/50 * * * * ?")
     public void test() {
         System.out.println("定时任务 === This is Scheduling 10s Task - " + new Date());
     }
--- src/main/java/com/cern/springboottest1/mapper/UserMapper.java
+++ src/main/java/com/cern/springboottest1/mapper/UserMapper.java
@@ -1,9 +1,11 @@
 package com.cern.springboottest1.mapper;
 
 import com.cern.springboottest1.domain.UserData;
+import org.apache.ibatis.annotations.CacheNamespace;
 import org.apache.ibatis.annotations.Mapper;
 import org.apache.ibatis.annotations.Select;
 
+@CacheNamespace(implementation = Mybatis)
 @Mapper
 public interface UserMapper {
 
